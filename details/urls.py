af78da619ae43bc4d95b05521ac76a101d441050
AnjanRanaMagar <98565821+AnjanRanaMagar@users.noreply.github.com>
2022-08-24 00:05:03 -0700

    Delete details directory

--- details/urls.py
+++
@@ -1,9 +0,0 @@
-
-from .import views
-# from django.contrib import admin
-from django.urls import path,include
-
-urlpatterns = [
-    path('',views.index, name='index'),
-    path('index_two',views.index_two, name='index_two'),
-]
