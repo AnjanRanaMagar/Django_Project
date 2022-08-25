250dc0f6c07f73e19656f58a0cdf265d0b922e36
AnjanRanaMagar <98565821+AnjanRanaMagar@users.noreply.github.com>
2022-08-24 17:05:44 -0700

    Delete project1 directory

--- project1/details/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Model1 as Student
-from .models import Model2 as Department
-
-# Register your models here. username = ANJAN, PSW: myWallet5$
-admin.site.register(Student),
-admin.site.register(Department),
