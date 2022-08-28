53763373e8f44764ef96434260fd4ad24289838b
AnjanRanaMagar <98565821+AnjanRanaMagar@users.noreply.github.com>
2022-08-28 16:23:57 -0700

    Update admin.py

--- Assignment_django_final/details/admin.py
+++ Assignment_django_final/details/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Student, Department
 
-# Register your models here. username = ANJAN, PSW: myWallet5$
+# Register your models here. 
 admin.site.register(Student),
 admin.site.register(Department),
