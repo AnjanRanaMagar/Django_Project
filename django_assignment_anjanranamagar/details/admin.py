b887e434e775886bc7e77df78a9fc02ce86df51e
AnjanRanaMagar <98565821+AnjanRanaMagar@users.noreply.github.com>
2022-08-30 17:43:53 -0700

    Delete django_assignment_anjanranamagar directory

--- django_assignment_anjanranamagar/details/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Student, Department
-
-# Register your models here. username = ANJAN, PSW: myWallet5$
-admin.site.register(Student),
-admin.site.register(Department),
